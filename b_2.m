0fbeeb772e960eaf090bf1bcde61aa57b953d797
zijunl <zijunlin@usc.edu>
2018-12-02 12:08:15 -0800

    Add files via upload

--- b_2.m
+++ b_2.m
@@ -5,23 +5,25 @@ linsysa = zeros(M-2,N);
 linsystemp = zeros(M,N-2);
 dx = 1/(M-1);
 dt = 1/(M-1);
+%write bottom line
 for i = 1:(M-1)
     for j = 1:N
-        u(M,j) = 1/5 * pi*(j-1);
+        u(M,j) = sin(1/5 * pi*(j-1));
     end
 end
+%write linearsystems
 for i = 1:(M-2)
     linsysa(i,i) = -dt/dx^2 ;
     linsysa(i,i+1) = 2*dt/dx^2 +1 ;
     linsysa(i,i+2) = -dt/dx^2 ;
 end
-
+%creat temp system
 for i = 1:M
     for j =1 :(N-2)
         linsystemp(i,j) = u(i,j+1);
     end
 end
-
+%calculate
 for j = 1:(N-1)
     u(M-j,:) = linsysa\(linsystemp(M-j+1,:))';
     for i = 1:M
